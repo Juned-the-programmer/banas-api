0ae4821590b81ad27030141db3513adcf09f108c
Juned-the-programmer <davdajuned7698@gmail.com>
2022-02-13 15:14:02 +0530

    Enhance the featrures

--- API/views.py
+++ API/views.py
@@ -9,9 +9,10 @@ from rest_framework import status
 
 from .serializer import *
 from datetime import date, timedelta
+import datetime
+import calendar
 
 from django.db.models import Sum,Min,Max,Avg
-from django.forms.models import model_to_dict
 # Create your views here.
 @api_view(['GET'])
 def login(request):
@@ -82,13 +83,71 @@ def list_customer(request):
 
 @api_view(['POST'])
 def add_daily_entry(request):
+    today_date = datetime.datetime.now()
+    year = today_date.year
+    month = today_date.month
+    day = today_date.day
+    last_date = calendar.monthrange(year, month)[0]
+
+    data = request.data
+    data_values = list(data.values())
+    pk = data_values[0]
+    # print(list(data.keys()))
+
     if request.method == 'POST':
         serializer = DailyEntrySerializer(data=request.data)
+
         if serializer.is_valid():
             serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
 
-        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+            if day == last_date:
+        
+                customer_name = Customer.objects.get(pk=pk).id
+
+                last_day_of_prev_month = date.today().replace(day=1) - timedelta(days=1)
+                start_day_of_prev_month = date.today().replace(day=1) - timedelta(days=last_day_of_prev_month.day)
+
+                last_day = last_day_of_prev_month.strftime("%Y-%m-%d")
+                start_day = start_day_of_prev_month.strftime("%Y-%m-%d")
+
+
+                total_cooler = DailyEntry.objects.filter(date__gte=start_day_of_prev_month , date__lte=last_day_of_prev_month).aggregate(Sum('cooler'))
+                coolers_total = total_cooler['cooler__sum']
+                
+                if coolers_total == None:   
+                    coolers_total = 0
+
+
+                last_month_due_amount = CustomerAccount.objects.get(customer_name = pk).due
+                rate = Customer.objects.get(pk=pk).rate
+
+                total = (int(coolers_total) * int(rate)) + int(last_month_due_amount)
+
+                bill_data = {
+                    'customer_name' : pk ,
+                    'to_date' : last_day ,
+                    'from_date' : start_day ,
+                    'coolers' : coolers_total ,
+                    'Pending_amount' : last_month_due_amount ,
+                    'Rate' : rate , 
+                    'Total' : total
+                }
+                print(bill_data)
+                data = bill_data
+                data_values = list(data.values())
+                pk = data_values[0]
+                # print(list(data.keys()))
+
+                bill_serializer = GenerateBillSerializer(data = bill_data)
+                if bill_serializer.is_valid():
+                    bill_serializer.save()
+                    customer = CustomerAccount.objects.get(customer_name=pk)
+                    customer.due = data_values[6]
+                    customer.save()
+
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_201_CREATED)
+    return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def daily_count(request):
@@ -107,9 +166,32 @@ def daily_count(request):
 def customer_payment(request):
     if request.method == 'POST':
         serializer = CustomerPaymentSerializer(data = request.data)
+        
+        # Conver the data to the list to get the each and every element from that data
+        data = request.data
+        data_values = list(data.values())
+        pk = data_values[0]
+        print(list(data.keys()))
+        # Ends here
+        
+        last_day_of_prev_month = date.today().replace(day=1) - timedelta(days=1)
+        start_day_of_prev_month = date.today().replace(day=1) - timedelta(days=last_day_of_prev_month.day)
+
+        last_day = last_day_of_prev_month.strftime("%Y-%m-%d")
+        start_day = start_day_of_prev_month.strftime("%Y-%m-%d")
+
         if serializer.is_valid():
             serializer.save()
+            customer = CustomerAccount.objects.get(customer_name = pk)
+            customer.due = int(data_values[1]) - int(data_values[2])
+            customer.save()
+            
+            customer_bill = CustomerBill.objects.filter(customer_name = pk).filter(paid=False).get(from_date=start_day)
+            customer_bill.paid = True
+            customer_bill.save()
+
             return Response(status=status.HTTP_201_CREATED)
+
 
         return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
 
@@ -117,7 +199,7 @@ def customer_payment(request):
 def customer_account(request , pk):
 
     try:
-        customer = CustomerAccount.objects.get(pk=pk)
+        customer = CustomerAccount.objects.get(customer_name = pk)
     except Customer.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -134,59 +216,20 @@ def due_list(request,pk):
     if request.method == 'GET':
 
         try:
-            customer = CustomerAccount.objects.get(pk=pk)
+            route = Route.objects.get(pk=pk)
         except Customer.DoesNotExist:
             return Response("Customer Route Not Found" , status=status.HTTP_404_NOT_FOUND)
-
-        customer_due = CustomerAccount.objects.filter(customer_name__id__in = Customer.objects.filter(route=pk))
-        serializer = CustomerAccountSerializer(customer_due ,many=True)
-        return Response(serializer.data , status=status.HTTP_200_OK)
-
-    return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
-def get_bill_data(request , pk):
-
-    try:
-        customer = Customer.objects.get(pk=pk)
-    except Customer.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        customer_name = Customer.objects.get(pk=pk).name
 
         last_day_of_prev_month = date.today().replace(day=1) - timedelta(days=1)
         start_day_of_prev_month = date.today().replace(day=1) - timedelta(days=last_day_of_prev_month.day)
 
-        total_cooler = DailyEntry.objects.filter(date__gte=start_day_of_prev_month , date__lte=last_day_of_prev_month).aggregate(Sum('cooler'))
-        coolers_total = total_cooler['cooler__sum']
-        
-        if coolers_total == None:   
-            coolers_total = 0
+        last_day = last_day_of_prev_month.strftime("%Y-%m-%d")
+        start_day = start_day_of_prev_month.strftime("%Y-%m-%d")
 
-        print(coolers_total)
+        # customer_due = CustomerAccount.objects.filter(customer_name__id__in = Customer.objects.filter(route=pk))
+        customer_due_list = CustomerBill.objects.filter(customer_name__id__in = Customer.objects.filter(route=pk)).filter(paid=False).filter(from_date=start_day)
+        # serializer = CustomerAccountSerializer(customer_due ,many=True)
+        serializer = GenerateBillSerializer(customer_due_list , many=True)
+        return Response(serializer.data , status=status.HTTP_200_OK)
 
-        last_month_due_amount = CustomerAccount.objects.get(customer_name = pk).due
-        rate = Customer.objects.get(pk=pk).rate
-
-        total = ( int(coolers_total) * int(rate) ) + int(last_month_due_amount)
-
-        return JsonResponse(
-            {'customer_name' : customer_name ,
-             'last_day' : last_day_of_prev_month ,
-             'start_day' : start_day_of_prev_month ,
-             'coolers' : coolers_total ,
-             'due_amount' : last_month_due_amount ,
-             'rate' : rate , 
-             'total' : total}
-        )
-
-@api_view(['POST'])
-def generate_bill(request):
-    if request.method == 'POST':
-        serializer = GenerateBillSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
