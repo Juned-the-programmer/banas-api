8d4af4ef7820dbf05745668feb5e3a8ab9ab895e
Juned-the-programmer <davdajuned7698@gmail.com>
2022-08-06 12:32:36 +0530

    Authentication

--- API/views.py
+++ API/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import *
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view , permission_classes
+from rest_framework.permissions import IsAuthenticated , IsAdminUser
 from rest_framework.response import Response
 from django.http import JsonResponse
 from django.core import serializers
@@ -19,6 +20,7 @@ def login(request):
     return JsonResponse("Login Page" , safe=False)
 
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
 def dashboard(request):
     return JsonResponse("Dashboard" , safe=False)
 
