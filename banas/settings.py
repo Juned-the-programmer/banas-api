a14c56b88382af2931600b266620f8164efa4345
Juned Davada <juneddavada@Juneds-MacBook-Air.local>
2025-09-07 22:16:32 +0530

    Added Region Name

--- banas/settings.py
+++ banas/settings.py
@@ -253,5 +253,5 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-# AWS_S3_REGION_NAME = "ap-south-1"  # example, change to your bucket’s region
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')  # example, change to your bucket’s region
 AWS_QUERYSTRING_AUTH = False  # makes public URLs without ?AWSAccessKeyId
